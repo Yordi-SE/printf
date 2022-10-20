bbf22d8f6f33bf3ab74151760490888c8f68c5a0
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2022-10-20 05:58:22 -0700

    string

--- main.h
+++ main.h
@@ -5,7 +5,7 @@ void print_array(int *array, int o);
 int _printf(const char *format, ...);
 int _print_strin(char *c, int *s, int *i);
 int _putchar(char c);
-int count_size(long int c);
+int count_size(unsigned int c);
 int _bool(const char *format, char *spec, int i);
 int _print_number2(int c);
 
